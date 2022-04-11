2a9fadcf76cdce856d7dccbb375942d0a0a2eac4
lcnr <rust@lcnr.de>
2022-04-11 08:28:49 +0200

    update `basic-consts` test

--- src/decl/test/basic-consts.rkt
+++ src/decl/test/basic-consts.rkt
@@ -21,7 +21,26 @@
     (CrateDecl (term (TheCrate (crate (TraitDecl AdtDecl_Foo ConstDecl_Malformed)))))
     (Env (term (env-for-crate-decl CrateDecl)))
     )
+   (traced '()
+           (decl:test-cannot-prove
+            Env
+            (crate-ok-goal (CrateDecl) CrateDecl)))
+   )
 
+  ;; Program:
+  ;;
+  ;; trait Debug { }
+  ;; struct Foo<T: Debug> { }
+  ;; const WF<T: Debug>: Foo<T>;
+  (redex-let*
+   formality-decl
+
+   ((TraitDecl (term (Debug (trait ((TyKind Self)) () ()))))
+    (AdtDecl_Foo (term (Foo (struct ((TyKind T)) ((Implemented (Debug (T)))) ((Foo ()))))))
+    (ConstDecl_Wf (term (Wf (const ((TyKind T)) ((Implemented (Debug (T)))) (TyRigid Foo (T))))))
+    (CrateDecl (term (TheCrate (crate (TraitDecl AdtDecl_Foo ConstDecl_Wf)))))
+    (Env (term (env-for-crate-decl CrateDecl)))
+    )
    (traced '()
            (decl:test-can-prove
             Env
